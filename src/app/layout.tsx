5ba2594d9b8f414dcb4316b6b50ccbc41f747b16
Jared Muñoz <amunozbaez669@gmail.com>
2024-08-19 18:37:57 -0600

    chore: Update file formatting and imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import { cn } from "@/core/utils"
-import { quicksand } from "@/core/config"
+import { quicksand } from "@/core/config";
+import { cn } from "@/core/utils";
 
-import "./globals.css"
+import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          quicksand.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					quicksand.variable,
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
