5150cd6d088f175d3637e667990428498926fabe
JosueVJ <josuevj.1e.49@gmail.com>
2024-09-11 00:22:06 -0600

    arreglando header. se añadieron los componentes de select, calendar, popover para hacer los componentes Select y PickerFecha. No es responsive aun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Home
-		</main>
-	);
+export default function Page({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+    //con searchParams podemos acceder a los parametros de la url pudiendo haber solo una cadena o un array de cadenas
+    //o incluso no haber parametros
+
+    const parametrosBusqueda = searchParams.a; //aqui accedemos al parametro a
+    return (
+        <div>
+            <h1>My Page</h1>
+            <p>
+                Query Param A: {Array.isArray(parametrosBusqueda) ? parametrosBusqueda.join(", ") : parametrosBusqueda}
+            </p>
+        </div>
+    );
 }
