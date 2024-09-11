f3d6cc714cff86d0a7dce10483ae51ea6829754e
JosueVJ <josuevj.1e.49@gmail.com>
2024-09-10 22:18:25 -0600

    subiendo dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,16 @@ import { quicksand } from "@/core/config";
 import { cn } from "@/core/utils";
 
 import "./globals.css";
+import Header from "@/components/ui/header";
 
-export default function RootLayout({
-	children,
-}: { children: React.ReactNode }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={cn(
-					"min-h-screen bg-background font-sans antialiased",
-					quicksand.variable,
-				)}
-			>
-				{children}
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head />
+            <body className={cn("min-h-screen bg-background font-sans antialiased", quicksand.variable)}>
+                <Header></Header>
+                {children}
+            </body>
+        </html>
+    );
 }
