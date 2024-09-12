7f84cd144aac4a7c9086f014c14e37826f8a660e
Jared Muñoz <amunozbaez669@gmail.com>
2024-09-11 18:30:41 -0600

    fix: use next Link component instead of an anchor

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { PickerFecha } from "../functional/pickerFecha";
 import { SelectCategoria } from "../functional/selectCategoria";
 import SearchBar from "../sports-filter";
@@ -6,18 +7,19 @@ export default function Header() {
 	return (
 		<header className="bg-gray-800 ">
 			<nav className="container flex items-center justify-between p-4">
-				<a href="/" className="text-white text-2xl font-bold">
+				<Link href="/" className="text-white text-2xl font-bold">
 					Are We Playing?
-				</a>
+				</Link>
 				<ul className="flex grow space-x-4 justify-center">
 					<li>
 						<SelectCategoria />
 					</li>
-					<li><SearchBar/></li>
+					<li>
+						<SearchBar />
+					</li>
 					<li>
 						<PickerFecha />
-					</li> 
-					
+					</li>
 				</ul>
 			</nav>
 		</header>
