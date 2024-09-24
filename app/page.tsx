917ae18234c0472319b922c9d3523a8adb91a020
Jared Muñoz <amunozbaez669@gmail.com>
2024-09-24 16:26:34 -0600

    fix: remove duplicate app folder

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CardWrapper from "@/components/ui/CardWeapper";
-
-export default function Page() {
-	return (
-		<div className="mx-5 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-			<CardWrapper></CardWrapper>
-		</div>
-	);
-}
