e22ec0e4adb73f5d7b35633630cea230e8ed1cab
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-02 19:57:54 -0600

    feat: add e2e testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,19 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-	await page.goto("http://localhost:3000");
-
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/AreWePlaying/);
+	await page.goto("http://localhost:3000/");
+	await expect(page.getByRole("link", { name: "AreWePlaying?" })).toBeVisible();
+	await expect(
+		page.locator("button").filter({ hasText: "Categoría" }),
+	).toBeVisible();
+	await page.getByText("#Varonil").first().click();
+	await expect(
+		page.locator("button").filter({ hasText: "Varonil" }),
+	).toBeVisible();
+	await page.getByRole("button", { name: "Limpiar" }).click();
+	await expect(
+		page.locator("button").filter({ hasText: "Categoría" }),
+	).toBeVisible();
+	await page.getByRole("button", { name: "CN" }).click();
+	await expect(page.getByLabel("CN")).toBeVisible();
 });
