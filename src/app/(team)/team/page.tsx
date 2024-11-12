eb81076bc1492905de3b271f8ff551ecf98f93e5
Jared Muñoz <amunozbaez669@gmail.com>
2024-11-12 16:15:58 -0600

    feat: update form styles

--- src/app/(team)/team/page.tsx
+++ src/app/(team)/team/page.tsx
@@ -1,67 +1,81 @@
 "use client";
 
+import ImagePreview from "@/components/image-preview";
 import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import { Input, InputContainer } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
-import ImageSelector from "@/components/image-selector"; // Importa el nuevo componente
 
 export default function TeamCreationPage() {
-  const [teamName, setTeamName] = useState('');
-  const [schoolName, setSchoolName] = useState('');
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+	const [teamName, setTeamName] = useState("");
+	const [schoolName, setSchoolName] = useState("");
+	const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Form submitted:', { teamName, schoolName, selectedImage });
-    // Reset form after submission
-    setTeamName('');
-    setSchoolName('');
-    setSelectedImage(null);
-  };
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log("Form submitted:", { teamName, schoolName, selectedImage });
+		// Reset form after submission
+		setTeamName("");
+		setSchoolName("");
+		setSelectedImage(null);
+	};
 
-  return (
-    <main className="w-screen flex justify-center items-center h-dvh overflow-y-auto">
-      <section className="w-full px-20 lg:w-auto lg:mx-auto flex flex-col gap-8">
-        <header className="flex flex-col items-center lg:items-start w-full justify-center relative">
-          <h2 className="text-4xl text-purple-500 font-medium text-start">
-            Registro de Equipo Nuevo
-          </h2>
-        </header>
-        <form onSubmit={handleSubmit} className="gap-4 sm:grid-cols-none">
-          <InputContainer>
-            <Label htmlFor="teamName">Nombre del Equipo</Label>
-            <Input 
-              type="text" 
-              id="teamName" 
-              placeholder="Nombre del equipo"
-              value={teamName}
-              onChange={(e) => setTeamName(e.target.value)}
-              required
-            />
-          </InputContainer>
-          <InputContainer>
-            <Label className="mt-4" htmlFor="schoolName">Nombre de la Escuela</Label>
-            <Input 
-              type="text" 
-              id="schoolName" 
-              placeholder="Nombre de la escuela"
-              value={schoolName}
-              onChange={(e) => setSchoolName(e.target.value)}
-              required
-            />
-          </InputContainer>
-            <InputContainer>
-            <Label htmlFor="schoolName" className="mt-6">Imagen del Equipo</Label>
-                <ImageSelector 
-                selectedImage={selectedImage} 
-                setSelectedImage={setSelectedImage} 
-                fallbackText="Selecciona una imagen para el equipo" 
-            />
-            </InputContainer>
-          <Button type="submit" className="mt-5 w-full">Registrar Equipo</Button>
-        </form>
-      </section>
-    </main>
-  );
+	return (
+		<main className="w-screen flex justify-center items-center h-dvh overflow-y-auto">
+			<Card className="max-w-md w-full">
+				<CardHeader>
+					<CardTitle className="text-purple-500">
+						Registro de Equipo Nuevo
+					</CardTitle>
+				</CardHeader>
+				<Separator />
+				<form onSubmit={handleSubmit}>
+					<CardContent className="grid grid-cols-[1fr_auto_auto] p-0">
+						<div className="p-6 flex flex-col gap-y-4">
+							<InputContainer>
+								<Label htmlFor="teamName">Nombre del Equipo</Label>
+								<Input
+									type="text"
+									id="teamName"
+									placeholder="Nombre del equipo"
+									value={teamName}
+									onChange={(e) => setTeamName(e.target.value)}
+									required
+								/>
+							</InputContainer>
+							<InputContainer className="row-start-2 row-end-3">
+								<Label htmlFor="schoolName">Nombre de la Escuela</Label>
+								<Input
+									type="text"
+									id="schoolName"
+									placeholder="Nombre de la escuela"
+									value={schoolName}
+									onChange={(e) => setSchoolName(e.target.value)}
+									required
+								/>
+							</InputContainer>
+						</div>
+						<Separator orientation="vertical" />
+						<div className="grid place-content-center p-4">
+							<ImagePreview fallback={teamName.at(0) ?? "K"} />
+						</div>
+					</CardContent>
+					<Separator />
+					<CardFooter className="mt-6">
+						<Button type="submit" className="ml-auto">
+							Registrar Equipo
+						</Button>
+					</CardFooter>
+				</form>
+			</Card>
+		</main>
+	);
 }
