d6c98ff0b7e20fa245b8acca101773ba411425a3
Jared Muñoz <amunozbaez669@gmail.com>
2024-11-20 11:45:11 -0600

    fix: add redirect to admin routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	distDir: "dist",
+	async redirects() {
+		return [
+			{
+				source: "/admin",
+				destination: "/admin/match",
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
