9dcd21a587f8a25e28f1cecd9dabcfebc7f29995
Jared Muñoz <amunozbaez669@gmail.com>
2025-10-20 22:54:18 -0600

    feat: add new TeamPosition type

--- src/types/team.ts
+++ src/types/team.ts
@@ -4,3 +4,33 @@ export interface Team {
 	logo?: string;
 	shortName: string;
 }
+
+export interface TeamPosition {
+	id: string;
+	position: number;
+	name: string;
+	shortName: string;
+	matches: number;
+	localMatches: number;
+	awayMatches: number;
+	percentage: number;
+	wr: number;
+	wins: number;
+	losses: number;
+	localWins: number;
+	localLosses: number;
+	awayWins: number;
+	awayLosses: number;
+	goalsFor: number;
+	goalsAgainst: number;
+	goalDifference: number;
+	localGoalsFor: number;
+	localGoalsAgainst: number;
+	localGoalDifference: number;
+	awayGoalsFor: number;
+	awayGoalsAgainst: number;
+	awayGoalDifference: number;
+	points: number;
+	localPoints: number;
+	awayPoints: number;
+}
