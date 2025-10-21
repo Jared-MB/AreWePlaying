2fce58fe5ff6e2209d39dda5a61eb6b54602fc26
Jared Muñoz <amunozbaez669@gmail.com>
2025-10-21 12:22:21 -0600

    fix: change cache life from get-match-day from days to max since this data will rarely changes

--- src/use-cases/get-match-days.ts
+++ src/use-cases/get-match-days.ts
@@ -7,7 +7,7 @@ import fs from "node:fs/promises";
 export async function getMatchDays(): Promise<MatchDay[]> {
 	"use cache";
 	cacheTag("match-days");
-	cacheLife("days");
+	cacheLife("max");
 	const matchDays = await fs.readFile("./src/assets/match-days.json", "utf-8");
 	return JSON.parse(matchDays);
 }
