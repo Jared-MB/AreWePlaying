ebcffdd8ad30275b03e404c235c2230bf13c3e7b
Jared Muñoz <amunozbaez669@gmail.com>
2025-10-22 21:04:19 -0600

    feat: change weeks version on mobile

--- src/components/schedule-filters.tsx
+++ src/components/schedule-filters.tsx
@@ -8,15 +8,32 @@ import { useSearchParams } from "next/navigation";
 import { PrefetchLink } from "./prefetch-link";
 import { SelectUniversity } from "./select-university";
 import { useCurrentWeek } from "@/hooks/use-current-week";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./ui/select";
+import { useRouter } from "next/navigation";
 
 export function ScheduleFilters({ weeks }: { weeks: MatchDay[] }) {
 	const week = useCurrentWeek(weeks);
 
 	const params = useSearchParams();
 
-	const selectedWeek = params.get("week") ?? week.currentWeek?.id;
+	const [selectedWeek, setSelectedWeek] = useState<string | undefined>(
+		params.get("week") ?? week.currentWeek?.id,
+	);
 
 	const [selectedLeague, setSelectedLeague] = useState<string>("femenil");
+
+	const router = useRouter();
+
+	const handleWeekChange = (week: string) => {
+		setSelectedWeek(week);
+		router.push(`?week=${week}`);
+	};
 
 	return (
 		<div className="mb-12 space-y-6">
@@ -77,12 +94,24 @@ export function ScheduleFilters({ weeks }: { weeks: MatchDay[] }) {
 					<span className="text-xs font-bold uppercase tracking-widest text-muted-foreground flex items-center mr-1">
 						Semana:
 					</span>
+					<Select value={selectedWeek} onValueChange={handleWeekChange}>
+						<SelectTrigger className="md:hidden !bg-primary border-2 [&>svg]:fill-primary-foreground border-foreground text-primary-foreground font-bold uppercase tracking-wider shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
+							<SelectValue placeholder="Selecciona una semana" />
+						</SelectTrigger>
+						<SelectContent>
+							{weeks.map((week) => (
+								<SelectItem key={week.id} value={week.id}>
+									{week.week}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
 					{weeks.map((week) => (
 						<Button
 							key={week.id}
 							size="sm"
 							asChild
-							className={`duration-300 hover:text-primary-foreground border-2 border-foreground font-bold uppercase tracking-wider shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none ${
+							className={`hidden md:inline-block duration-300 hover:text-primary-foreground border-2 border-foreground font-bold uppercase tracking-wider shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none ${
 								selectedWeek === week.id
 									? "bg-foreground text-background"
 									: "bg-background text-foreground"
@@ -93,7 +122,6 @@ export function ScheduleFilters({ weeks }: { weeks: MatchDay[] }) {
 					))}
 				</div>
 			</div>
-
 			<div className="h-[4px] bg-foreground" />
 		</div>
 	);
@@ -138,6 +166,11 @@ export function ScheduleFiltersSkeleton() {
 					<span className="text-xs font-bold uppercase tracking-widest text-muted-foreground flex items-center mr-1">
 						Semana:
 					</span>
+					<Select>
+						<SelectTrigger className="md:hidden">
+							<SelectValue placeholder="Selecciona una semana" />
+						</SelectTrigger>
+					</Select>
 					{Array.from({ length: 18 }, (_, i) => ({
 						id: i + 1,
 						week: `Week ${i + 1}`,
@@ -145,7 +178,7 @@ export function ScheduleFiltersSkeleton() {
 						<Button
 							key={week.id}
 							size="sm"
-							className="duration-300 hover:text-primary-foreground border-2 border-foreground font-bold uppercase tracking-wider shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none bg-background text-foreground"
+							className="hidden md:inline-block duration-300 hover:text-primary-foreground border-2 border-foreground font-bold uppercase tracking-wider shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none bg-background text-foreground"
 						>
 							Semana {week.id}
 						</Button>
